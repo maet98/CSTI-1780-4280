ee222d4c999fbd8cdf883765321a9132057bd4dc
Miguel Estevez <maletaveras@gmail.com>
2021-09-18 10:41:12 -0400

    fixed layout

--- MyFirstApp/app/src/main/java/com/pucmm/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/pucmm/myfirstapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.pucmm.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public fun onClickSend(view: View) {
+        println("Hi there")
     }
 }
