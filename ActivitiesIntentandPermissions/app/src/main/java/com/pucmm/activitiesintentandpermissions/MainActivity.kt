7faa8b4fe4976a5777a6c62f70095fadae74788d
Miguel Estevez <maletaveras@gmail.com>
2021-09-30 15:17:13 -0400

    Added new view with logic

--- ActivitiesIntentandPermissions/app/src/main/java/com/pucmm/activitiesintentandpermissions/MainActivity.kt
+++ ActivitiesIntentandPermissions/app/src/main/java/com/pucmm/activitiesintentandpermissions/MainActivity.kt
@@ -1,11 +1,128 @@
 package com.pucmm.activitiesintentandpermissions
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Switch
+import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var permissionList = mutableListOf<String>(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.CAMERA,
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.READ_CONTACTS
+        );
+        var switchs = mutableListOf<Switch>(
+            findViewById<Switch>(R.id.storageSwitch),
+            findViewById<Switch>(R.id.locationSwitch),
+            findViewById<Switch>(R.id.cameraSwitch),
+            findViewById<Switch>(R.id.phoneSwitch),
+            findViewById<Switch>(R.id.contactSwitch)
+        )
+
+        for ( i in permissionList.indices) {
+            if (ActivityCompat.checkSelfPermission(this, permissionList[i]) == PackageManager.PERMISSION_GRANTED) {
+                switchs[i].isChecked = true;
+            }
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                switchs[i].setTextColor(this.getColor(R.color.white))
+            };
+        }
+        val continueButton = findViewById<Button>(R.id.continueButton);
     }
+
+
+    private fun requestPermission() {
+        val permissionList = mutableListOf<String>(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.CAMERA,
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.READ_CONTACTS
+        );
+
+        val switchs = mutableListOf<Switch>(
+                findViewById<Switch>(R.id.storageSwitch),
+                findViewById<Switch>(R.id.locationSwitch),
+                findViewById<Switch>(R.id.cameraSwitch),
+                findViewById<Switch>(R.id.phoneSwitch),
+                findViewById<Switch>(R.id.contactSwitch)
+        )
+        val arr = mutableListOf<String>();
+        for (i in switchs.indices) {
+            if (switchs[i].isChecked) {
+                arr.add(permissionList[i])
+            }
+        }
+        if (arr.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, arr.toTypedArray(), 0);
+        }
+    }
+
+
+    public fun nextPage(view: View) {
+        requestPermission()
+        val intent = Intent(this, TestPermission::class.java)
+        startActivityForResult(intent, 0);
+    }
+
+    public fun checkPermission(view: View) {
+        when(view.id) {
+            R.id.storageSwitch -> {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    val mySnackbar = Snackbar.make(view, "Permission Granted!!!", Snackbar.LENGTH_SHORT)
+                    mySnackbar.show()
+                    val switch = findViewById<Switch>(view.id)
+                    switch.isChecked = true
+                }
+            }
+            R.id.locationSwitch -> {
+                if( ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    val mySnackbar = Snackbar.make(view,"Permission Granted!!!", Snackbar.LENGTH_SHORT)
+                    mySnackbar.show()
+                    val switch = findViewById<Switch>(view.id)
+                    switch.isChecked = true
+                }
+
+            }
+            R.id.cameraSwitch -> {
+                if( ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    val mySnackbar = Snackbar.make(view,"Permission Granted!!!", Snackbar.LENGTH_SHORT)
+                    mySnackbar.show()
+                    val switch = findViewById<Switch>(view.id)
+                    switch.isChecked = true
+                }
+
+            }
+            R.id.phoneSwitch -> {
+                if( ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                    val mySnackbar = Snackbar.make(view,"Permission Granted!!!", Snackbar.LENGTH_SHORT)
+                    mySnackbar.show()
+                    val switch = findViewById<Switch>(view.id)
+                    switch.isChecked = true
+                }
+
+            }
+            R.id.contactSwitch -> {
+                if( ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+                    val mySnackbar = Snackbar.make(view,"Permission Granted!!!", Snackbar.LENGTH_SHORT)
+                    mySnackbar.show()
+                    val switch = findViewById<Switch>(view.id)
+                    switch.isChecked = true
+                }
+
+            }
+        }
+    }
+
 }
---
+++ ActivitiesIntentandPermissions/app/src/main/java/com/pucmm/activitiesintentandpermissions/TestPermission.kt
@@ -0,0 +1,87 @@
+package com.pucmm.activitiesintentandpermissions
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.ContactsContract
+import android.provider.MediaStore
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
+
+class TestPermission : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_permission)
+    }
+
+    public fun launchGoogleMaps(view: View) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            // Create a Uri from an intent string. Use the result to create an Intent.
+            val gmmIntentUri = Uri.parse("geo:37.7749,-122.4194")
+
+            // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            // Make the Intent explicit by setting the Google Maps package
+            mapIntent.setPackage("com.google.android.apps.maps")
+
+            // Attempt to start an activity that can handle the Intent
+            startActivity(mapIntent)
+        } else {
+            var mySnackbar = Snackbar.make(view, "You need to grant location permission first", Snackbar.LENGTH_SHORT);
+            mySnackbar.show()
+        }
+    }
+
+    public fun launchContacts(view: View) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent(Intent.ACTION_PICK).apply {
+                type = ContactsContract.Contacts.CONTENT_TYPE
+            }
+            startActivity(intent)
+        } else {
+            var mySnackbar = Snackbar.make(view, "You need to grant contacts permission first", Snackbar.LENGTH_SHORT);
+            mySnackbar.show()
+        }
+    }
+
+
+    public fun launchPhone(view: View) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "829-960-2769"))
+            startActivity(intent)
+        } else {
+            var mySnackbar = Snackbar.make(view, "You need to grant phone permission first", Snackbar.LENGTH_SHORT);
+            mySnackbar.show()
+        }
+    }
+
+
+    public fun launchStorage(view: View) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+                type = "image/*"
+            }
+            startActivity(intent)
+        } else {
+            var mySnackbar = Snackbar.make(view, "You need to grant storage permission first", Snackbar.LENGTH_SHORT);
+            mySnackbar.show()
+        }
+    }
+
+    public fun launchCamera(view: View) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            val camera_intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            val pic_id = 123;
+            startActivityForResult(camera_intent, pic_id);
+        } else {
+            var mySnackbar = Snackbar.make(view, "You need to grant camera permission first", Snackbar.LENGTH_SHORT);
+            mySnackbar.show()
+        }
+    }
+}
